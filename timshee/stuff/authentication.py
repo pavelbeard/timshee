927a1078ff5110efc289bc7850eb54acc953362d
pavelbeard <pavelborodin1995@gmail.com>
2024-04-24 13:03:25 +0200

    fifteenth commit

---
+++ timshee/stuff/authentication.py
@@ -0,0 +1,19 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from rest_framework.exceptions import ValidationError
+
+User = get_user_model()
+
+
+class EmailAuthenticationBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if username is None or password is None:
+            return None
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+
+        except User.DoesNotExist:
+            return ValidationError("User not found")
+        return None
