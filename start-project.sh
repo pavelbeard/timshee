6ee5d64d992ac6b56607a7e9f1c42a0dfecf2e84
pavelbeard <pavelborodin1995@gmail.com>
2024-06-25 16:09:56 +0200

    fortieth-third commit. STABLE
    fixed bugs
    
    SAVE

--- start-project.sh
+++ start-project.sh
@@ -10,4 +10,5 @@ cp timshee/prod.env.copy timshee/prod.env
 cp postgres-files/prod.env.copy postgres-files/prod.env
 
 pip download -r timshee/requirements.txt -d timshee/packages;
+docker-compose down;
 docker-compose up --build -d;
--- timshee/store/admin.py
+++ timshee/store/admin.py
@@ -1,10 +1,32 @@
+import sys
+from io import BytesIO
+
+from PIL import Image
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from . import models
 
 
 # Register your models here.
+
+
+def compress_image(image):
+    tmp = Image.open(image)
+    output_to_stream = BytesIO()
+    image_temp = tmp.convert('RGB')
+    image_temp.save(output_to_stream, format='JPEG', quality=85)
+    output_to_stream.seek(0)
+    file = InMemoryUploadedFile(
+        output_to_stream, 'ImageField',
+        f'{image.name.split(".")[0]}.jpg',
+        'image/jpeg',
+        sys.getsizeof(output_to_stream),
+        None
+    )
+    return file
+
 
 @admin.register(models.Item)
 class ItemAdmin(admin.ModelAdmin):
@@ -15,11 +37,19 @@ class ItemAdmin(admin.ModelAdmin):
         model = models.CarouselImage
 
     inlines = [StockInLine, CarouselImageInline]
+    
+    def save_model(self, request, obj, form, change):
+        if obj.image:
+            obj.image = compress_image(obj.image)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(models.CarouselImage)
 class StockImageAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        if obj.image:
+            obj.image = compress_image(obj.image)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(models.Type)
@@ -48,11 +78,19 @@ class CategoryAdmin(admin.ModelAdmin):
         model = models.Type
 
     inlines = [TypeInline]
+    
+    def save_model(self, request, obj, form, change):
+        if obj.category_image:
+            obj.category_image = compress_image(obj.category_image)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(models.Collection)
 class CollectionAdmin(admin.ModelAdmin):
-    pass
+    def save_model(self, request, obj, form, change):
+        if obj.collection_image:
+            obj.collection_image = compress_image(obj.collection_image)
+        super().save_model(request, obj, form, change)
 
 
 @admin.register(models.Wishlist)
