0ceeb84028c5be936bfef0c74056b46a4dfafa5e
pavelbeard <pavelborodin1995@gmail.com>
2024-08-24 19:37:46 +0200

    version 1.0.71
    UNSTABLE
    
    Updated A LOT OF CODE
    I made him more stable

--- unstable.creator.sh
+++ unstable.creator.sh
@@ -1,4 +1,5 @@
 #!/bin/zsh
 docker-compose -f unstable.creator.docker-compose.yml up -d --build;
-docker build -t pavelbeard/frontend:unstable -f ./frontend/unstable.Dockerfile ./frontend;
-docker build -t pavelbeard/django:unstable -f ./timshee/unstable.Dockerfile ./timshee;
+docker buildx create --name timshee_builder --use;
+docker buildx build --platform linux/amd64,linux/arm64 -t pavelbeard/frontend:unstable -f ./frontend/unstable.Dockerfile ./frontend;
+docker buildx build --platform linux/amd64,linux/arm64 -t pavelbeard/django:unstable -f ./timshee/unstable.Dockerfile ./timshee;
