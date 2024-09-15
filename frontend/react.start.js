fd4078b038a5bd43844f171c867a1838811a991c
pavelbeard <pavelborodin1995@gmail.com>
2024-09-15 19:23:50 +0200

    Version 1.0.887 UNSTABLE I have written a proxy server and improve django serializers

--- frontend/react.start.js
+++ frontend/react.start.js
@@ -1,17 +1,30 @@
 const express = require('express');
-const path = require('path');
 const app = express();
+const path = require('path');
 const port = 3000;
 const { createProxyMiddleware } = require('http-proxy-middleware');
 require('dotenv').config();
 
-app.use('/api', createProxyMiddleware({
-    target: `${process.env.DRF_API_URL}/api`,
+const url = process.env.DRF_API_URL + '/api'
+const proxy = createProxyMiddleware({
+    target: url,
     changeOrigin: true,
-    onProxyReq: (proxyReq) => {
-        proxyReq.setHeader('X-Api-Key', process.env.DRF_API_KEY)
+    onError: (err, req, res) => {
+        console.error('Proxy error:', err);
+        res.status(500).send('Proxy error');
+    },
+    onProxyReq: (proxyReq, req, res) => {
+        console.log(`Proxying request: ${req.url}`);
+        // Uncomment if you want to set additional headers
+        // proxyReq.setHeader('X-Api-Key', process.env.DRF_API_KEY);
+    },
+    onProxyRes: (proxyRes, req, res) => {
+        console.log(`Received response with status: ${proxyRes.statusCode}`);
     }
-}));
+})
+console.log('DRF_API_URL=', url);
+
+app.use('/api', proxy);
 
 app.use(express.static(path.join(__dirname, 'build')));
 
