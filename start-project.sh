b12b2463a283c525b0b8f3155347071f49b96e18
pavelbeard <pavelborodin1995@gmail.com>
2024-09-17 10:07:26 +0200

    Version 1.0.896 STABLE -> to main Role of API key keeper will serve the load_balancer

--- start-project.sh
+++ start-project.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-cp timshee/prod.env timshee/prod.env.copy;
-cp postgres-files/prod.env postgres-files/prod.env.copy;
+cp .env env.copy;
 
 git stash;
 git pull;
 
-cp timshee/prod.env.copy timshee/prod.env
-cp postgres-files/prod.env.copy postgres-files/prod.env
+cp .env.copy .env;
 
-pip download -r timshee/requirements.txt -d timshee/packages;
 docker-compose down;
 docker-compose up --build -d;
