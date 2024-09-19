c1fea0cc1924d217c28eaf5d60a9ccf2441ce90d
pavelbeard <pavelborodin1995@gmail.com>
2024-09-19 10:08:38 +0200

    Version 1.0.94 STABLE
    Mini fixes

--- prod.start-project.sh
+++ prod.start-project.sh
@@ -7,5 +7,9 @@ git pull;
 
 cp .env.copy .env
 
+docker pull pavelbeard/django.production:latest;
+docker pull pavelbeard/file_storage.production:latest;
+docker pull pavelbeard/frontend.production:latest;
+
 docker-compose -f prod.docker-compose.yml down;
 docker-compose -f prod.docker-compose.yml --env-file .env up -d;
